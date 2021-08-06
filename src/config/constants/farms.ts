829c67029ff4c31ae9358a18e08d7db5fb72f95e
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-08-06 20:21:37 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -4,28 +4,28 @@ import { FarmConfig } from './types'
 const farms: FarmConfig[] = [
   {
     pid: 1,
-    lpSymbol: 'SEKO-BNB LP',
+    lpSymbol: 'SEKO-MATIC LP',
     lpAddresses: {
-      56: '0x8e7FCb9a44037568ce4752A1B9107D6AF12C1D33',
+      56: '',
       137: '',
     },
     token: tokens.seko,
-    quoteToken: tokens.wbnb,
+    quoteToken: tokens.wmatic,
   },
   {
     pid: 2,
-    lpSymbol: 'SEKO-BUSD LP',
+    lpSymbol: 'SEKO-USDT LP',
     lpAddresses: {
-      56: '0xFEfEB231cb443e5DdB905B7cCD564a65b98d5E91',
+      56: '',
       137: '',
     },
     token: tokens.seko,
-    quoteToken: tokens.busd,
+    quoteToken: tokens.usdt,
   },
  
   {
     pid: 3,
-    lpSymbol: 'BNB-BUSD LP',
+    lpSymbol: 'MATIC-USDC LP',
     lpAddresses: {
       56: '0x58f876857a02d6762e0101bb5c46a8c1ed44dc16', // WBNB-BUSD
       137: '0x6e7a5fafcec6bb1e78bae2a1f0b612012bf14827', // WMATIC-USDC
@@ -33,38 +33,38 @@ const farms: FarmConfig[] = [
     token: tokens.busd,
     quoteToken: tokens.wbnb,
   },
-  {
-    pid: 12,
-    lpSymbol: 'USDT-BUSD LP',
-    lpAddresses: {
-      56: '0x7efaef62fddcca950418312c6c91aef321375a00',
-      137: '',
-    },
-    token: tokens.busd,
-    quoteToken: tokens.usdt,
-  },
+//   {
+//     pid: 12,
+//     lpSymbol: 'USDT-BUSD LP',
+//     lpAddresses: {
+//       56: '0x7efaef62fddcca950418312c6c91aef321375a00',
+//       137: '',
+//     },
+//     token: tokens.busd,
+//     quoteToken: tokens.usdt,
+//   },
   
-  {
-    pid: 5,
-    lpSymbol: 'BTCB-BNB LP',
-    lpAddresses: {
-      56: '0x61eb789d75a95caa3ff50ed7e47b96c132fec082', // BTCB-WBNB
-      137: '0xdC9232E2Df177d7a12FdFf6EcBAb114E2231198D', // WBTC-WETH
-    },
-    token: tokens.btcb,
-    quoteToken: tokens.wbnb,
-  },
+//   {
+//     pid: 5,
+//     lpSymbol: 'BTCB-BNB LP',
+//     lpAddresses: {
+//       56: '0x61eb789d75a95caa3ff50ed7e47b96c132fec082', // BTCB-WBNB
+//       137: '0xdC9232E2Df177d7a12FdFf6EcBAb114E2231198D', // WBTC-WETH
+//     },
+//     token: tokens.btcb,
+//     quoteToken: tokens.wbnb,
+//   },
   
-  {
-    pid: 6,
-    lpSymbol: 'ETH-BNB LP',
-    lpAddresses: {
-      56: '0x74e4716e431f45807dcf19f284c7aa99f18a4fbc', // ETH-WBNB
-      137: '0xadbf1854e5883eb8aa7baf50705338739e558e5b', // WMATIC-WETH
-    },
-    token: tokens.eth,
-    quoteToken: tokens.wbnb,
-  },
- ]
+//   {
+//     pid: 6,
+//     lpSymbol: 'ETH-BNB LP',
+//     lpAddresses: {
+//       56: '0x74e4716e431f45807dcf19f284c7aa99f18a4fbc', // ETH-WBNB
+//       137: '0xadbf1854e5883eb8aa7baf50705338739e558e5b', // WMATIC-WETH
+//     },
+//     token: tokens.eth,
+//     quoteToken: tokens.wbnb,
+//   },
+//  ]
 
 export default farms
