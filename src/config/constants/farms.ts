a062b94256dbad0c58d81240eafdd03e2a544b15
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-08-06 19:35:48 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -33,38 +33,38 @@ const farms: FarmConfig[] = [
     token: tokens.busd,
     quoteToken: tokens.wbnb,
   },
-//    {
-//     pid: 4,
-//     lpSymbol: 'USDT-BUSD LP',
-//     lpAddresses: {
-//       56: '0x58f876857a02d6762e0101bb5c46a8c1ed44dc16',
-//       137: '',
-//     },
-//     token: tokens.busd,
-//     quoteToken: tokens.usdt,
-//   },
+  {
+    pid: 4,
+    lpSymbol: 'USDT-BUSD LP',
+    lpAddresses: {
+      56: '0x58f876857a02d6762e0101bb5c46a8c1ed44dc16',
+      137: '',
+    },
+    token: tokens.busd,
+    quoteToken: tokens.usdt,
+  },
   
-//   {
-//     pid: 5,
-//     lpSymbol: 'BTCB-BNB LP',
-//     lpAddresses: {
-//       56: '0x61eb789d75a95caa3ff50ed7e47b96c132fec082', // BTCB-WBNB
-//       137: '0xdC9232E2Df177d7a12FdFf6EcBAb114E2231198D', // WBTC-WETH
-//     },
-//     token: tokens.btcb,
-//     quoteToken: tokens.wbnb,
-//   },
+  {
+    pid: 5,
+    lpSymbol: 'BTCB-BNB LP',
+    lpAddresses: {
+      56: '0x61eb789d75a95caa3ff50ed7e47b96c132fec082', // BTCB-WBNB
+      137: '0xdC9232E2Df177d7a12FdFf6EcBAb114E2231198D', // WBTC-WETH
+    },
+    token: tokens.btcb,
+    quoteToken: tokens.wbnb,
+  },
   
-//   {
-//     pid: 6,
-//     lpSymbol: 'ETH-BNB LP',
-//     lpAddresses: {
-//       56: '0x74e4716e431f45807dcf19f284c7aa99f18a4fbc', // ETH-WBNB
-//       137: '0xadbf1854e5883eb8aa7baf50705338739e558e5b', // WMATIC-WETH
-//     },
-//     token: tokens.eth,
-//     quoteToken: tokens.wbnb,
-//   },
+  {
+    pid: 6,
+    lpSymbol: 'ETH-BNB LP',
+    lpAddresses: {
+      56: '0x74e4716e431f45807dcf19f284c7aa99f18a4fbc', // ETH-WBNB
+      137: '0xadbf1854e5883eb8aa7baf50705338739e558e5b', // WMATIC-WETH
+    },
+    token: tokens.eth,
+    quoteToken: tokens.wbnb,
+  },
  ]
 
 export default farms
