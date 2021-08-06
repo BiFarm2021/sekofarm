e9408883f08fc646bf6b9018449a69387ffbecc6
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-08-06 11:39:17 +0700

    Update fetchFarmUser.ts

--- src/state/farms/fetchFarmUser.ts
+++ src/state/farms/fetchFarmUser.ts
@@ -76,7 +76,7 @@ export const fetchFarmUserEarnings = async (account: string, farmsToFetch: FarmC
   const calls = farmsToFetch.map((farm) => {
     return {
       address: masterChefAddress,
-      name: 'pendingBiYield',
+      name: 'pendingSeko',
       params: [farm.pid, account],
     }
   })
