6f126a61a61ee136c0b5125c334bbb74a5f1d4f8
KaloDefi <87683746+KaloDefi@users.noreply.github.com>
2021-08-07 10:06:16 +0700

    Update index.ts

--- src/state/prices/index.ts
+++ src/state/prices/index.ts
@@ -13,8 +13,7 @@ export const fetchPrices = createAsyncThunk<PriceApiThunk>('prices/fetch', async
   const response = await fetch('https://api.pancakeswap.info/api/tokens')
   const data = (await response.json()) as PriceApiResponse
 
-  const response2 = await fetch('https://api.pancakeswap.info/api/v2/tokens')
-  const data2 = (await response2.json()) as PriceApiResponse
+
 
   // Return normalized token names
   const prices = {
